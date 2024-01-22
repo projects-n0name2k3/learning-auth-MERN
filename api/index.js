1f639e025589ea0990d70dc2a70a3c53c89d4490
Noname2k4 <nva.tiktok90@gmail.com>
2024-01-22 16:54:18 +0700

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL)
@@ -14,3 +15,5 @@ const app = express();
 app.listen(3002, () => {
   console.log("Server listening on 3002");
 });
+
+app.use("/api/user", userRoute);
