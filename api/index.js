868d10083b17accf9fcaff641b93afd60de60808
Noname2k4 <nva.tiktok90@gmail.com>
2024-01-22 17:13:19 +0700

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoute from "./routes/user.route.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL)
@@ -12,8 +13,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3002, () => {
   console.log("Server listening on 3002");
 });
 
-app.use("/api/user", userRoute);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
