b75617e15a39f62df634a54d4b69133a50856a62
Noname2k4 <nva.tiktok90@gmail.com>
2024-02-05 16:26:16 +0700

    update header component and private route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -10,7 +13,19 @@ const Header = () => {
         <ul className="flex gap-4">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
-          <Link to="/login">Sign In</Link>
+          {currentUser ? (
+            <Link to="/profile">
+              <img
+                src={currentUser?.profilePicture}
+                className="rounded-full object-cover"
+                width={"30px"}
+                height={"30px"}
+                alt=""
+              />
+            </Link>
+          ) : (
+            <Link to="/login">Sign In</Link>
+          )}
         </ul>
       </div>
     </div>
