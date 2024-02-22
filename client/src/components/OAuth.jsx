fab1f6f74a941d288991d1186eb7c6e93aa5ab3d
Noname2k4 <nva.tiktok90@gmail.com>
2024-02-22 21:58:24 +0700

    hot fix

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,8 +3,10 @@ import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 const OAuth = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -22,8 +24,8 @@ const OAuth = () => {
         }),
       });
       const data = await res.json();
-      console.log(data);
       dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
       console.log("Cound not login with google");
     }
